590b1983ffe6d71c39e9505c6f44816d23aa2cd4
hiroki <haohuirixia851@gmail.com>
2024-12-21 16:26:12 +0900

    Update user profile api

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -1,9 +1,11 @@
 import express from "express"
-import { createCurrentUser } from "../controllers/createCurrentUser";
-import { jwtCheck } from "../middleware/auth";
+import { createCurrentUser, updateCurrentUser } from "../controllers/createCurrentUser";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 router.post("/",jwtCheck, createCurrentUser)
+router.put("/",jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser)
 
 export default router
