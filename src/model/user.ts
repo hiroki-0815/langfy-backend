7693d3177115b3da6363711da175e086b66afe68
hiroki <haohuirixia851@gmail.com>
2024-12-21 16:25:43 +0900

    a

--- src/model/user.ts
+++ src/model/user.ts
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
-  imageUrl:{type: String},
+  // imageUrl:{type: String},
 });
 
 const User = mongoose.model("User", userSchema);
