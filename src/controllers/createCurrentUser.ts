077b870d064e041ab002adc91d900f046ae95c59
hiroki <haohuirixia851@gmail.com>
2024-12-23 10:03:06 +0900

    add age

--- src/controllers/createCurrentUser.ts
+++ src/controllers/createCurrentUser.ts
@@ -1,33 +1,44 @@
-import {Request, RequestHandler, Response} from "express"
+import { Request, RequestHandler, Response } from "express";
 import User from "../model/user";
 
-export const createCurrentUser: RequestHandler = async(req:Request, res: Response): Promise<void>=>{
-try {
-  const {auth0Id} = req.body;
-  const existingUser = await User.findOne({auth0Id})
-
-  if(existingUser) {
-   res.status(200).send();
-   return
-  }
-
-  const newUser = new User(req.body)
-  await newUser.save();
-
-  res.status(201).json(newUser.toObject())
-} catch (error) {
-  console.log(error);
-  res.status(500).json({message: "Error creating user"})
-}
-}
-
-export const updateCurrentUser: RequestHandler =async (req:Request, res: Response): Promise<void>  => {
+export const createCurrentUser: RequestHandler = async (req: Request, res: Response): Promise<void> => {
   try {
-    const {name, gender, city, country, learningLanguage, fluencyLevel, motivation,selfIntroduction} = req.body
-    const user = await User.findById(req.userId)
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
 
-    if(!user){
-      res.status(404).json({message: "User not found"})
+    if (existingUser) {
+      res.status(200).send();
+      return;
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
+};
+
+export const updateCurrentUser: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const {
+      name,
+      gender,
+      city,
+      country,
+      age,
+      learningLanguage,
+      fluencyLevel,
+      motivation,
+      selfIntroduction,
+    } = req.body;
+
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
       return;
     }
 
@@ -35,14 +46,17 @@ export const updateCurrentUser: RequestHandler =async (req:Request, res: Respons
     user.gender = gender;
     user.city = city;
     user.country = country;
+    user.age = age;
     user.learningLanguage = learningLanguage;
     user.fluencyLevel = fluencyLevel;
     user.motivation = motivation;
     user.selfIntroduction = selfIntroduction;
 
+    await user.save();
+
     res.send(user);
   } catch (error) {
     console.log(error);
-    res.status(500).json({message: "Error updating user"})
+    res.status(500).json({ message: "Error updating user" });
   }
-}
+};
