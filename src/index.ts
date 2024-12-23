a669644b4e7ea18a093e96af674126f11359a178
hiroki <haohuirixia851@gmail.com>
2024-12-23 16:44:32 +0900

    first diploy

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import "dotenv/config"
 import mongoose from "mongoose";
 import myUserRoute from './routes/myUserRoute'
 
+const PORT = process.env.PORT || 7001;
+
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
   console.log("Connected to database");
   
@@ -13,8 +15,12 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute)
 
-app.listen(7001, ()=>{
-  console.log("sever started on localhost:7001");
-})
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`);
+});    
