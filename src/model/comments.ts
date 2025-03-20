c8fc2b5ffce3a99dc000a7f92fa3396aabcd77fd
hiroki <haohuirixia851@gmail.com>
2025-03-20 12:57:15 +0900

    build model for comment

---
+++ src/model/comments.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
