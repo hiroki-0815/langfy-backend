4b35e695aef5f1b027c30c371294e919e3751db0
hiroki <haohuirixia851@gmail.com>
2025-03-21 11:45:57 +0900

    build post route

---
+++ src/routes/postRoute.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import { createPost, deletePost, getAllPosts, likePost, unlikePost, } from "../controllers/posts.controller";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+
+const router = express.Router();
+
+router.get('/',
+jwtCheck,jwtParse,
+getAllPosts)
+
+router.post('/',
+jwtCheck,jwtParse,
+ createPost)
+
+router.post('/:postId/like',
+jwtCheck,jwtParse,
+ likePost)
+
+router.post('/:postId/unlike',
+jwtCheck,jwtParse,
+ unlikePost)
+
+router.delete('/:postId/',
+jwtCheck,jwtParse,
+deletePost)
+
+export default router;
